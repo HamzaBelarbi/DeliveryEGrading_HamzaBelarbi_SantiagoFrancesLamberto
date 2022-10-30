b6aebace33da4d82be8db664f9d97211cd51a13f
Santiago Francés Lamberto <hdhdhdhdxdYT@gmail.com>
2022-10-30 13:56:28 +0100

    actualización "dupe"

---
+++ M05_UF3_P1/Model_design/Game.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Game
+{
+
+    //Variables
+
+    private int id;
+    private int productId;
+    private float rating;
+    private string version;
+
+    private bool update;
+    private bool add;
+    private bool remove;
+
+    //Funciones
+    
+    void Start()
+    {
+
+
+    }
+
+    void Update()
+    {
+
+
+
+    }
+    public void GameNumber(int _productId)
+    {
+
+    }
+
+    public void UpdateGame(bool _update)
+    {
+
+    }
+
+    public void AddGame(bool _add)
+    {
+
+    }
+
+    public void RemoveGame(bool _remove)
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+}
